138b00914a65ef5c405ccc3017d23d761ac4ee3e
francisgill42 <francisgill42@gmail.com>
2020-08-18 12:15:53 +0500

    added forward in image

--- pages/login.vue
+++ pages/login.vue
@@ -13,7 +13,7 @@ Close
 <v-card class="elevation-10 pa-3">
 <v-card-text>
 <div class="layout column align-center">
-<img src="energy-logo.png" alt="ssep login">
+<img src="/energy-logo.png" alt="ssep login">
 <!-- <h1 class="flex my-2 secondary--text">SSEP LOGIN</h1> -->
 </div>
 <v-form>
